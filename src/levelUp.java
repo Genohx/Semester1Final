9e354b59b6ba511241ca307f90076987ec610812
Genohx <jmswineford@gmail.com>
2019-12-13 15:41:50 -0800

    hoping this commit works?

--- src/levelUp.java
+++ src/levelUp.java
@@ -1,4 +1,4 @@
 public interface levelUp
 {
-    public void level();//called when either the player or the enemies level up
+    public void level();//called when either the player or the enemies level upp
 }
